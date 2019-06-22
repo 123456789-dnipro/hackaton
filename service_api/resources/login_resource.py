d8c4d645d13cd2757eb1d4fe2dff6ef149a7f9f4
Ovsiienko Mykyta <Ovsiienko Mykyta>
2019-06-22 18:36:48 +0300

    hot fix

--- service_api/resources/login_resource.py
+++ service_api/resources/login_resource.py
@@ -17,8 +17,7 @@ class LogInResource(BaseResource):
 
     async def post(self, request):
         data, _ = LogInForm().load(request.json)
-        check_request = users.select().where(users.c.phone == data['phone'])
-        if await pg.fetchrow(check_request):
+        if await data.get('conf_code'):
             return await self.__login(data)
 
         return await self.__registration(data)
