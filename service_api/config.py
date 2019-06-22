f61f9a8ee200e8a4501c1f0c9d1cd641e9f4aa8e
Ovsiienko Mykyta <Ovsiienko Mykyta>
2019-06-22 11:52:27 +0300

    heroku integration

--- service_api/config.py
+++ service_api/config.py
@@ -2,12 +2,12 @@ import os
 
 
 class AppConfig:
-    DB_NAME = os.getenv('DB_NAME', 'chats')
-    DB_HOST = os.getenv('DB_HOST', 'localhost')
+    DB_NAME = os.getenv('DB_NAME', 'parking')
+    DB_HOST = os.getenv('DATABASE_URL', 'localhost')
     DB_PORT = os.getenv('DB_PORT', '5432')
     DB_USER = os.getenv('DB_USER', 'su_admin')
     DB_PASSWORD = os.getenv('DB_PASSWORD', 'pass')
-    REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
+    REDIS_HOST = os.getenv('REDIS_URL', 'localhost')
     REDIS_PORT = os.getenv('REDIS_PORT', '6379')
     REDIS_USER = os.getenv('REDIS_USER', 'user')
     REDIS_PASSWORD = os.getenv('REDIS_PORT', 'pass')
