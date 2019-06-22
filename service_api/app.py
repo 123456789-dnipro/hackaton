d1de913c360991b718e97d3db00c9b8953c11262
Heorhii <gosha1104@nxt.ru>
2019-06-22 14:32:49 +0300

    Merge branch 'development' of /Users/a1/python_projects/hackaton with conflicts.

--- service_api/app.py
+++ service_api/app.py
@@ -1,5 +1,3 @@
-from asyncpgsa import pg
-
 from sanic import Sanic
 
 from service_api import api
@@ -13,13 +11,18 @@ app.config.from_object(AppConfig)
 api.load_api(app)
 
 
-@app.listener('after_server_start')
-async def setup_db(app, loop):
-    await get_pool()
-    await redis.init(app)
-
-
-@app.listener('before_server_stop')
-async def clone_connection(app, loop):
-    await pg.pool.close()
-    await redis.close()
+# @app.listener('after_server_start')
+# async def setup_db(app, loop):
+#     app.pool = await get_pool()
+#     await redis.init(app)
+#
+#
+# @app.listener('before_server_stop')
+# async def clone_connection(app, loop):
+#     await app.pool.close()
+#     await redis.close()
+#
+#
+# @app.middleware('request')
+# async def add_engine(request):
+#     request['pool'] = app.pool
