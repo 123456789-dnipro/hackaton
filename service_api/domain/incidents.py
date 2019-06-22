ab652e17dfc0f6af0ce1e4a91ad04dd1f23c4aa7
Heorhii <gosha1104@nxt.ru>
2019-06-22 14:24:28 +0300

    add requirements

--- service_api/domain/incidents.py
+++ service_api/domain/incidents.py
@@ -2,8 +2,8 @@ def get_incidents(headers, longitude, latitude):
     return 'OK'
 
 
-def report_incident(headers, data, longitude, latitude):
-    pass
+def report_incident(headers, longitude, latitude, image, car_number, comment):
+    return 'OK', 200
 
 
 def get_incident(headers, incident_id):
