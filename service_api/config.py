bbe9b68afffd85356c2bdd7dd97bf739b0b00c6a
Ovsiienko Mykyta <Ovsiienko Mykyta>
2019-06-22 13:02:13 +0300

    heroku fix

--- service_api/config.py
+++ service_api/config.py
@@ -2,8 +2,9 @@ import os
 
 
 class AppConfig:
+    DB_URI = os.getenv('DATABASE_URL')
     DB_NAME = os.getenv('DB_NAME')
-    DB_HOST = os.getenv('DATABASE_URL', 'localhost')
+    DB_HOST = os.getenv('DB_HOST', 'localhost')
     DB_PORT = os.getenv('DB_PORT')
     DB_USER = os.getenv('DB_USER')
     DB_PASSWORD = os.getenv('DB_PASSWORD')
--- service_api/domain/db.py
+++ service_api/domain/db.py
@@ -30,14 +30,13 @@ async def get_pool():
     from service_api.app import app
     pool = app.pool if hasattr(app, 'pool') else None
     if not pool:
-        db_creds = {
-            # 'user': app.config.DB_USER,
-            # 'password': app.config.DB_PASSWORD,
+        db_creds = app.config.DB_URI or {
+            'user': app.config.DB_USER,
+            'password': app.config.DB_PASSWORD,
             'host': app.config.DB_HOST,
-            # 'port': app.config.DB_PORT,
-            # 'database': app.config.DB_NAME,
-            'ssl': True
+            'port': app.config.DB_PORT,
+            'database': app.config.DB_NAME,
         }
-        await pg.init(**db_creds)
+        await pg.init(db_creds)
         pool = await asyncpgsa.create_pool(**db_creds)
     return pool
