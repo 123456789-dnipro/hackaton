ac18b7f3a0c9680a7b3e2842dd364f05e1439cad
Ovsiienko Mykyta <Ovsiienko Mykyta>
2019-06-22 12:21:10 +0300

    Deleted db creds

--- service_api/config.py
+++ service_api/config.py
@@ -2,15 +2,12 @@ import os
 
 
 class AppConfig:
-    DB_NAME = os.getenv('DB_NAME', 'parking')
+    DB_NAME = os.getenv('DB_NAME')
     DB_HOST = os.getenv('DATABASE_URL', 'localhost')
-    DB_PORT = os.getenv('DB_PORT', '5432')
-    DB_USER = os.getenv('DB_USER', 'su_admin')
-    DB_PASSWORD = os.getenv('DB_PASSWORD', 'pass')
+    DB_PORT = os.getenv('DB_PORT')
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
     REDIS_HOST = os.getenv('REDIS_URL', 'localhost')
-    REDIS_PORT = os.getenv('REDIS_PORT', '6379')
-    REDIS_USER = os.getenv('REDIS_USER', 'user')
-    REDIS_PASSWORD = os.getenv('REDIS_PORT', 'pass')
 
 
 class DevConfig(AppConfig):
