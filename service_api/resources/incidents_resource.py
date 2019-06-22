8ba9c00fed20535d2376db6b9521a88a50cfb64e
Heorhii <gosha1104@nxt.ru>
2019-06-22 19:18:03 +0300

    add get incedents

--- service_api/resources/incidents_resource.py
+++ service_api/resources/incidents_resource.py
@@ -2,29 +2,30 @@ from http import HTTPStatus
 from sanic.response import json
 
 from service_api.resources import BaseResource
-from service_api.domain.incidents import (
-    get_incidents,
-    report_incident,
-    change_incident_status,
-    get_incident
-)
+from service_api.domain.incidents import Incedent
 from service_api.domain.forms import IncidentsStatusForm
 from service_api.domain.decorators import prepare_coordinates
 
 
 class IncidentsResource(BaseResource):
-    decorators = [prepare_coordinates]
+    decorators = []
 
+    @prepare_coordinates
     async def get(self, request, longitude, latitude):
-        incidents = await get_incidents(longitude, latitude)
+        incidents = Incedent.get_incidents(request.headers, longitude, latitude)
         return json(incidents, HTTPStatus.OK)
 
-    async def post(self, request, longitude, latitude):
+    async def post(self, request):
         image = request.files.get('image')
         car_number = request.form.get('plate_number')
         comments = request.form.get('comments')
-        print(image)
-        incident, status = report_incident(request.headers, longitude, latitude, image, car_number, comments)
+        longitude = float(request.form.get('lon'))
+        latitude = float(request.form.get('lat'))
+        incident, status = await Incedent(headers=request.headers).report_incident(
+                                                    longitude=longitude,
+                                                    latitude=latitude, image=image,
+                                                    car_number=car_number,
+                                                    comment=comments)
         return json(incident, status)
 
 
@@ -32,10 +33,10 @@ class IncidentResource(BaseResource):
     decorators = []
 
     async def get(self, request, incident_id):
-        incident = await get_incident(incident_id)
+        incident = await Incedent.get_incident(incident_id)
         return json(incident, HTTPStatus.OK)
 
     async def put(self, request):
         data, _ = IncidentsStatusForm().load(request.json)
-        incident, status = change_incident_status(request.headers, data)
+        incident, status = Incedent.change_incident_status(request.headers, data)
         return json(incident, status)
